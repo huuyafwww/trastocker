95e73c97bca5c4ac3c118f26f86cd7c5418b6040
huuyafwww <huuya1234fwww@gmail.com>
2024-12-13 23:59:27 +0900

    create Logo and Logo components

---
+++ apps/app-frontend-server/src/components/shared/LogoTrastocker/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+import LogoImage from './logo.png';
+
+const LogoTrastocker = () => {
+  return <Image src={LogoImage} alt="Trastocker" />;
+};
+
+export default LogoTrastocker;
