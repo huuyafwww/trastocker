4b4e7fc8fe5a4493102218b08baaa4e709a3b2b2
huuyafwww <huuya1234fwww@gmail.com>
2024-12-15 01:21:31 +0900

    fix storybook output settings

--- apps/app-frontend-server/.storybook/main.ts
+++ apps/app-frontend-server/.storybook/main.ts
@@ -3,6 +3,8 @@ import { merge } from 'webpack-merge';
 
 import type { StorybookConfig } from '@storybook/nextjs';
 
+const maxAssetSize = 1024 * 128; // 128KB
+
 const config: StorybookConfig = {
   stories: ['../src/**/stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -14,9 +16,18 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['./public'],
-  webpackFinal: config =>
-    merge(config, {
-      plugins: [new VanillaExtractPlugin()],
-    }),
+  webpackFinal: config => merge(config, {
+    plugins: [new VanillaExtractPlugin()],
+
+    // @see https://webpack.js.org/plugins/split-chunks-plugin/
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30 * 1024, // 30KB
+        maxSize: maxAssetSize,
+      },
+    },
+    performance: { maxAssetSize },
+  }),
 };
 export default config;
