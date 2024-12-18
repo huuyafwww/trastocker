cbc4dee2d81d43d9b573261ff6c1d041f27c862c
huuyafwww <huuya1234fwww@gmail.com>
2024-12-18 23:43:14 +0900

    define loginUser mutation query and type

---
+++ apps/app-frontend-server/src/components/domains/LoginForm/gql.ts
@@ -0,0 +1,16 @@
+import { gql } from 'urql';
+
+import type { IMutation, IMutationLoginUserArgs } from '@trastocker/graphql-definition';
+
+export const loginUserMutation = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+export type LoginUserMutation = IMutation['loginUser'];
+export type LoginUserMutationVariables = IMutationLoginUserArgs;
