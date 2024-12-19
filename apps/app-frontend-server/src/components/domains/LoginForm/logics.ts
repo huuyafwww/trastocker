5eaef3a0ab2bfad6f811065ed0fda0d96ba7290c
huuyafwww <huuya1234fwww@gmail.com>
2024-12-19 22:49:47 +0900

    ローカルdev起動コマンド整備

--- apps/app-frontend-server/src/components/domains/LoginForm/logics.ts
+++ apps/app-frontend-server/src/components/domains/LoginForm/logics.ts
@@ -3,7 +3,12 @@ import { useCallback } from 'react';
 import { valibotResolver } from '@hookform/resolvers/valibot';
 import { UserEmailSchema, UserPasswordSchema } from '@trastocker/validation-schema-definition';
 import { useForm } from 'react-hook-form';
+import { useMutation } from 'urql';
 import * as v from 'valibot';
+
+import { loginUserMutation } from './gql';
+
+import type { LoginUserMutation, LoginUserMutationVariables } from './gql';
 
 type LoginFormValues = {
   email: string;
@@ -19,10 +24,13 @@ export const useLoginForm = () => {
   const methods = useForm<LoginFormValues>({
     resolver: valibotResolver(schema),
   });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [data, login] = useMutation<LoginUserMutation, LoginUserMutationVariables>(loginUserMutation);
 
-  const handleSubmit = useCallback((data: LoginFormValues) => {
-    console.log(data);
-  }, []);
+  const handleSubmit = useCallback(async (data: LoginFormValues) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const result = await login(data);
+  }, [login]);
 
   return {
     methods,
