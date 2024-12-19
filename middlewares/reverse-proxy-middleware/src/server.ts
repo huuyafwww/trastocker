1df2b88d071cae83f3b1fc1441130363b8fc76d2
huuyafwww <huuya1234fwww@gmail.com>
2024-12-19 22:15:24 +0900

    add reverse-proxy-middleware package

---
+++ middlewares/reverse-proxy-middleware/src/server.ts
@@ -0,0 +1,29 @@
+import fastifyHttpProxy from '@fastify/http-proxy';
+import fastify from 'fastify';
+
+const app = fastify({
+  ignoreDuplicateSlashes: true,
+  ignoreTrailingSlash: true,
+});
+
+// to app-backend-server
+app.register(fastifyHttpProxy, {
+  upstream: 'http://localhost:8787',
+  prefix: '/graphql',
+  rewritePrefix: '/graphql',
+});
+
+// to app-frontend-server
+app.register(fastifyHttpProxy, {
+  upstream: 'http://localhost:3000',
+  prefix: '/',
+  rewritePrefix: '/',
+});
+
+app.listen({ port: 80 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Reverse proxy is running on ${address}`);
+});
