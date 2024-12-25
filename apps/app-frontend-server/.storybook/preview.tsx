04b67ea5e4b8d53798c55e573d55ed5cea43f63d
huuyafwww <huuya1234fwww@gmail.com>
2024-12-25 22:08:52 +0900

    mockつなぎこみ

--- apps/app-frontend-server/.storybook/preview.tsx
+++ apps/app-frontend-server/.storybook/preview.tsx
@@ -6,15 +6,18 @@ import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'urql';
 
 import useUrql from '@/hooks/useUrql';
+import { setupDatabase, setupHandlers } from '@/mocks';
 
 import type { Preview } from '@storybook/react';
 
 initialize();
 
+const database = setupDatabase();
+
 const preview: Preview = {
   loaders: [mswLoader],
   parameters: {
-    msw: { handlers: [] },
+    msw: { handlers: setupHandlers(database) },
     controls: {
       matchers: {
         color: /(background|color)$/i,
