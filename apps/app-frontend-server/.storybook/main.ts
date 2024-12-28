7c1929da6520969cd33f93f4a23975c75ac5cfd7
huuyafwww <huuya1234fwww@gmail.com>
2024-12-28 23:02:54 +0900

    remove split-chunks setting, because slow build

--- apps/app-frontend-server/.storybook/main.ts
+++ apps/app-frontend-server/.storybook/main.ts
@@ -3,8 +3,6 @@ import { merge } from 'webpack-merge';
 
 import type { StorybookConfig } from '@storybook/nextjs';
 
-const maxAssetSize = 1024 * 1024; // 1MB
-
 const config: StorybookConfig = {
   stories: ['../src/**/stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -19,16 +17,6 @@ const config: StorybookConfig = {
   staticDirs: ['./public'],
   webpackFinal: config => merge(config, {
     plugins: [new VanillaExtractPlugin()],
-
-    // @see https://webpack.js.org/plugins/split-chunks-plugin/
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-        minSize: 30 * 1024, // 30KB
-        maxSize: maxAssetSize,
-      },
-    },
-    performance: { maxAssetSize },
   }),
 };
 export default config;
