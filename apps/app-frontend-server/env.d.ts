0a229abbfa58e4c4368c56aa66a7f5abb7056533
huuyafwww <huuya1234fwww@gmail.com>
2025-01-07 21:45:01 +0900

    NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL不要なので削除

--- apps/app-frontend-server/env.d.ts
+++ apps/app-frontend-server/env.d.ts
@@ -2,5 +2,4 @@
 // by running `wrangler types --env-interface CloudflareEnv env.d.ts`
 
 interface CloudflareEnv {
-  NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL: string;
 }
--- apps/app-frontend-server/types/env.d.ts
+++ apps/app-frontend-server/types/env.d.ts
@@ -1,5 +1,4 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL: string;
   }
 }
