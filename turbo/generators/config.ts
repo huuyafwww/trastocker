457d82311c83350974f890ea57057245ee7fe59b
huuyafwww <huuya1234fwww@gmail.com>
2025-03-11 23:12:03 +0900

    package generator

---
+++ turbo/generators/config.ts
@@ -0,0 +1,54 @@
+import type { PlopTypes } from '@turbo/gen';
+
+const PACKAGE_TYPES = {
+  app: 'apps',
+  definition: 'definitions',
+  helper: 'helpers',
+  middleware: 'middlewares',
+};
+
+type PackageType = keyof typeof PACKAGE_TYPES;
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setHelper('package', (type: PackageType) => PACKAGE_TYPES[type]);
+  plop.setHelper('library', (type: PackageType, name: string) => ({
+    app: `app-${name}-server`,
+    definition: `${name}-definition`,
+    helper: `${name}-helper`,
+    middleware: `${name}-middleware`,
+  }[type]));
+
+  plop.setGenerator('package', {
+    description: 'Create a new package',
+    prompts: [
+      {
+        type: 'list',
+        name: 'type',
+        message: 'What type of package do you want to create?',
+        choices: ['app', 'definition', 'helper', 'middleware'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your package name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '{{package type}}/{{library type name}}/package.json',
+        templateFile: 'templates/package.json.hbs',
+      },
+      {
+        type: 'add',
+        path: '{{package type}}/{{library type name}}/eslint.config.ts',
+        templateFile: 'templates/eslint.config.ts',
+      },
+      {
+        type: 'add',
+        path: '{{package type}}/{{library type name}}/tsconfig.json',
+        templateFile: 'templates/tsconfig.json',
+      },
+    ],
+  });
+}
