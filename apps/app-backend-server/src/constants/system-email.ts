4fa3ee401a05cc4d9e45d09d03f2c5a359ff5209
huuyafwww <huuya1234fwww@gmail.com>
2025-04-11 22:40:36 +0900

    system_emailのconstant修正

--- apps/app-backend-server/src/constants/system-email.ts
+++ apps/app-backend-server/src/constants/system-email.ts
@@ -1,3 +1,11 @@
-export const SYSTEM_EMAIL = {
-  register: process.env.NOREPLY_EMAIL,
-} as const;
+export const SYSTEM_EMAIL_KEYS = ['register'] as const;
+
+export type SYSTEM_EMAIL_TYPE = {
+  register: typeof SYSTEM_EMAIL['register'];
+};
+
+export class SYSTEM_EMAIL {
+  static get register() {
+    return process.env.NOREPLY_EMAIL;
+  }
+}
