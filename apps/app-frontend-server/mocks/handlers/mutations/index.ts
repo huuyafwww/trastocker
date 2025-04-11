b81f85f2a503703dd7fe8c2d351ffcdfeb9fc2b4
huuyafwww <huuya1234fwww@gmail.com>
2025-04-11 22:41:34 +0900

    mutationのmock実装

--- apps/app-frontend-server/mocks/handlers/mutations/index.ts
+++ apps/app-frontend-server/mocks/handlers/mutations/index.ts
@@ -2,6 +2,7 @@ import { createWorkspace } from './createWorkspace';
 import { joinWorkspace } from './joinWorkspace';
 import { loginUser } from './loginUser';
 import { registerUser } from './registerUser';
+import { verifyUser } from './verifyUser';
 
 import type { CreateHandler } from '../';
 
@@ -10,4 +11,5 @@ export const createMutationHandlers: CreateHandler[] = [
   registerUser,
   createWorkspace,
   joinWorkspace,
+  verifyUser,
 ];
