9d11cca7c07ed4f32056469c692f76952192b385
huuyafwww <huuya1234fwww@gmail.com>
2025-04-15 23:35:34 +0900

    tailwind v4 update and daisyuiv5 update

--- apps/app-frontend-server/tailwind.config.ts
+++ apps/app-frontend-server/tailwind.config.ts
@@ -1,14 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config = {
-  content: [
-    './src/**/*.{ts,tsx}',
-  ],
   theme: {
     extend: {},
   },
-  darkMode: 'class',
-  plugins: [require('daisyui')],
 } satisfies Config;
 
 export default config;
--- apps/app-frontend-server/vite.config.ts
+++ apps/app-frontend-server/vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
@@ -17,6 +18,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    tailwindcss(),
     react(),
   ],
   test: {
