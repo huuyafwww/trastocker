5093fd3d5a629ba270fc9c18bc8d65d6a30b7fa1
huuyafwww <huuya1234fwww@gmail.com>
2025-07-12 00:20:31 +0900

    build script修正

--- apps/app-preview-server/scripts/build.ts
+++ apps/app-preview-server/scripts/build.ts
@@ -6,7 +6,9 @@ const rootPath = `${process.cwd()}/../..`;
 await $`cd ${rootPath}/apps/app-frontend-server && pnpm run generate:mock-database && pnpm run storybook:build`;
 
 // Copy the built Storybook to the app-preview-server
+await $`mkdir ${rootPath}/apps/app-preview-server/dist/storybook/app-frontend-server`;
 await $`cp -r ${rootPath}/apps/app-frontend-server/storybook-static/* ${rootPath}/apps/app-preview-server/dist/storybook/app-frontend-server`;
 
 // copy the built UI elements to the app-preview-server
+await $`mkdir ${rootPath}/apps/app-preview-server/dist/storybook/ui-elements`;
 await $`cp -r ${rootPath}/libraries/ui-elements/storybook-static/* ${rootPath}/apps/app-preview-server/dist/storybook/ui-elements`;
