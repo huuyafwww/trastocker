67c514c8cbd545985ed81ca74bf91499c78056d8
huuyafwww <huuya1234fwww@gmail.com>
2025-09-12 00:24:44 +0900

    fix deploy path

--- apps/app-preview-server/src/components/pages/PreviewListPage/logics.ts
+++ apps/app-preview-server/src/components/pages/PreviewListPage/logics.ts
@@ -5,9 +5,9 @@ export const usePreviewLinks = (): {
   tbls: string;
 } => {
   return {
-    appFrontendServer: 'storybook/app-frontend-server',
-    uiElements: 'storybook/ui-elements',
-    liam: 'erd/liam',
+    appFrontendServer: 'storybook/app-frontend-server/',
+    uiElements: 'storybook/ui-elements/',
+    liam: 'erd/liam/',
     tbls: `https://github.com/huuyafwww/trastocker/blob/${process.env.NEXT_PUBLIC_GIT_COMMIT_REF}/definitions/database-definition/dbdoc/README.md`,
   };
 };
