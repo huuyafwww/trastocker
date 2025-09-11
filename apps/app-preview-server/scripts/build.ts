7a01a552769f45af758d3a996a321991d7382dd3
huuyafwww <huuya1234fwww@gmail.com>
2025-09-11 23:17:54 +0900

    script追加

--- apps/app-preview-server/scripts/build.ts
+++ apps/app-preview-server/scripts/build.ts
@@ -34,7 +34,7 @@ const rootPath = `${process.cwd()}/../..`;
 // Generate mock database and build Storybook for the app-frontend-server
 await $`cd ${rootPath}/apps/app-frontend-server && pnpm run generate:mock-database && pnpm run storybook:build`;
 
-// Copy the built Storybook to the app-frontend-server
+// Copy the built Storybook to the app-preview-server
 await $`mkdir -p ${rootPath}/apps/app-preview-server/dist/storybook/app-frontend-server`;
 await $`cp -r ${rootPath}/apps/app-frontend-server/storybook-static/* ${rootPath}/apps/app-preview-server/dist/storybook/app-frontend-server`;
 storybookConfigBaseUri(`${rootPath}/apps/app-preview-server/dist/storybook/app-frontend-server/index.html`, basePaths.appFrontendServerBasePath);
@@ -42,7 +42,14 @@ storybookConfigBaseUri(`${rootPath}/apps/app-preview-server/dist/storybook/app-f
 // Generate mock database and build Storybook for the app-ui-elements
 await $`cd ${rootPath}/libraries/ui-elements && pnpm run storybook:build`;
 
-// copy the built UI elements to the app-ui-elements
+// copy the built UI elements to the app-preview-server
 await $`mkdir -p ${rootPath}/apps/app-preview-server/dist/storybook/ui-elements`;
 await $`cp -r ${rootPath}/libraries/ui-elements/storybook-static/* ${rootPath}/apps/app-preview-server/dist/storybook/ui-elements`;
 storybookConfigBaseUri(`${rootPath}/apps/app-preview-server/dist/storybook/ui-elements/index.html`, basePaths.uiElementsBasePath);
+
+// Generate static page for Liam ERD
+await $`cd ${rootPath}/definitions/database-definition && pnpm run generate::liam-static-page`;
+
+// Copy the generated static page to the app-preview-server
+await $`mkdir -p ${rootPath}/apps/app-preview-server/dist/erd/liam`;
+await $`cp -r ${rootPath}/definitions/database-definition/liam/* ${rootPath}/apps/app-preview-server/dist/erd/liam`;
--- apps/app-preview-server/src/components/pages/PreviewListPage/logics.ts
+++ apps/app-preview-server/src/components/pages/PreviewListPage/logics.ts
@@ -7,7 +7,7 @@ export const usePreviewLinks = (): {
   return {
     appFrontendServer: 'storybook/app-frontend-server',
     uiElements: 'storybook/ui-elements',
-    liam: `https://liambx.com/erd/p/github.com/huuyafwww/trastocker/blob/${process.env.NEXT_PUBLIC_GIT_COMMIT_REF}/definitions/database-definition/schema.json`,
+    liam: 'erd/liam',
     tbls: `https://github.com/huuyafwww/trastocker/blob/${process.env.NEXT_PUBLIC_GIT_COMMIT_REF}/definitions/database-definition/dbdoc/README.md`,
   };
 };
